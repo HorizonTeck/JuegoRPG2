9dce79ec591010efbeaf82c2f8c014df1c3d8d37
HorizonTeck <marcoschurri@gmail.com>
2024-04-04 14:23:34 +0200

    cambios en vectores de definicion

--- personajes.h
+++ personajes.h
@@ -14,12 +14,12 @@ protected:
 public:
     Personajes();
     Personajes(const string& name);
-    Personajes(const string& name, const int atributos[],const Objetos **inventario);
+    Personajes(const string& name, const vector<int> atributos,const vector<Objetos*> inventario);
     string getName() const;
     vector<int> getAtributos() const;
     vector<Objetos*> getInventario() const;
     void setName(const string& name);
-    virtual void setAtributos(const int atributos[], const int tamaño) =0;
+    virtual void setAtributos(const vector<int> atributos, const int tamaño) =0;
     virtual void setAtributos() =0;
     virtual void setInventario(const Objetos* objetos[]) =0;
     int tirar_dados() const;
